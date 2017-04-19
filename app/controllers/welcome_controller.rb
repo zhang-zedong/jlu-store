e3725f84bad9fc75ba4d158b60d9124ba3e33fe6
zedong <lycczzd@gmail.com>
2017-04-19 18:10:25 +0800

    implement flashes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-
+    flash[:notice] = "success"
   end
 end
--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,3 +1,10 @@
 module FlashesHelper
-  
+  def user_facing_flashes
+    flash.to_hash.slice "notice", "alert", "warning", "info"
+  end
+
+  FLASH_CLASSES = {notice: "success", alert: "danger", warning: "warning", info: "info"}.freeze
+  def flash_class(key)
+    FLASH_CLASSES.fetch(key.to_sym, key)
+  end
 end
