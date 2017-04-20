319c1ee33f0a18f443cbde550e991484fdc180a8
zedong <lycczzd@gmail.com>
2017-04-20 14:33:54 +0800

    render total_price in cart

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,5 @@
 module CartsHelper
+  def render_total_price(current_cart)
+    current_cart.total_price
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items, as: :products
+
+  def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.product.price.present?
+        sum += cart_item.quantity * cart_item.product.price
+      end
+    end
+    sum
+  end
 end
